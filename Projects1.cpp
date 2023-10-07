e5c05b064d2a755646ab33c64705963daee18c9b
Mikun1994 <123875727+Mikun1994@users.noreply.github.com>
2023-10-07 12:45:29 +0530

    Projects1.cpp

--- Projects1.cpp
+++ Projects1.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
 
 int main() {
-    string name;
-    int age;
-    string email;
+    // Collect job application information
+    string fullName, email, phoneNumber, desiredPosition, qualifications;
 
-    // Ask the user for their name
-    cout << "Please enter your name: ";
-    getline(cin, name);
+    cout << "Job Application Form" << endl;
+    cout << "====================" << endl;
+    
+    cout << "Full Name: ";
+    getline(cin, fullName);
 
-    // Ask the user for their age
-    cout << "Please enter your age: ";
-    cin >> age;
-    cin.ignore(); // Ignore the newline character left in the input buffer
-
-    // Ask the user for their email address
-    cout << "Please enter your email address: ";
+    cout << "Email Address: ";
     getline(cin, email);
 
-    // Display the registration information
-    cout << "\nThank you for registering!\n";
-    cout << "Name: " << name << endl;
-    cout << "Age: " << age << endl;
-    cout << "Email: " << email << endl;
+    cout << "Phone Number: ";
+    getline(cin, phoneNumber);
+
+    cout << "Desired Position: ";
+    getline(cin, desiredPosition);
+
+    cout << "Qualifications: ";
+    getline(cin, qualifications);
+
+    // Create a text file with the application details
+    ofstream outFile("job_application.txt");
+    if (outFile.is_open()) {
+        outFile << "Job Application Details" << endl;
+        outFile << "=======================" << endl;
+        outFile << "Full Name: " << fullName << endl;
+        outFile << "Email Address: " << email << endl;
+        outFile << "Phone Number: " << phoneNumber << endl;
+        outFile << "Desired Position: " << desiredPosition << endl;
+        outFile << "Qualifications: " << qualifications << endl;
+        outFile.close();
+        cout << "Job application saved as job_application.txt." << endl;
+    } else {
+        cerr << "Error: Unable to open the file for writing." << endl;
+    }
 
     return 0;
 }
+
